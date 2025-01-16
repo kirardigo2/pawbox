8af71e5c6d0ba1950c476278fde35275589e87ce
kirardigo2 <gabrielfigueroa06@gmail.com>
2025-01-15 21:53:11 -0300

    traduccion interfaz y layout

--- script.js
+++ script.js
@@ -19,6 +19,20 @@ function loadContent() {
         document.getElementById('interfaz').textContent = translations[currentLanguage].interfaz;
         document.getElementById('codigo').textContent = translations[currentLanguage].codigo;
         document.getElementById('quienes').textContent = translations[currentLanguage].quienes;
+        
+        if (document.getElementById('interfaz_title')) {
+            document.getElementById('interfaz_title').textContent = translations[currentLanguage].interfaz_title;
+        }
+        if (document.getElementById('interfaz_texto')) {
+            document.getElementById('interfaz_texto').textContent = translations[currentLanguage].interfaz_texto;
+        }
+        if (document.getElementById('layout_title')) {
+            document.getElementById('layout_title').textContent = translations[currentLanguage].layout_title;
+        }
+        if (document.getElementById('layout_texto')) {
+            document.getElementById('layout_texto').textContent = translations[currentLanguage].layout_texto;
+        }
+
         document.querySelector('.language-btn').textContent = currentLanguage === 'es' ? 'English' : 'Español';
     }
 }
